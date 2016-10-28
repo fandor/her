f6443919f50c9e3ca3a7415a4654a1748e566cd2
Ted McElwee <ted.mcelwee@fandor.com>
2016-10-27 20:48:42 -0700

    remove logging

--- lib/her/model/relation.rb
+++ lib/her/model/relation.rb
@@ -6,11 +6,7 @@ module Her
 
       # @private
       def initialize(parent)
-        # Rails.logger.debug("***********************************")
-        # Rails.logger.debug("Her:Model:Relation: parent: #{parent.inspect}")
-        # @parent = parent
         @parent = parent.to_s.demodulize.constantize
-        # Rails.logger.debug("Her:Model:Relation: @parent: #{@parent.inspect}")
         @params = {}
       end
 
